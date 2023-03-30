d351a4f8920735f0e4a0f070aef22766994698ec
ALexFPVNoorth <121861188+ALexFPVNoorth@users.noreply.github.com>
2023-03-30 21:01:40 +0800

    upload

--- data.js
+++ data.js
@@ -1,8 +1,8 @@
 var APP_DATA = {
   "scenes": [
     {
-      "id": "0-gate-entrance",
-      "name": "gate, entrance",
+      "id": "0--",
+      "name": "Орц, гарц",
       "levels": [
         {
           "tileSize": 256,
@@ -37,14 +37,14 @@ var APP_DATA = {
           "yaw": 3.013484739839125,
           "pitch": 0.12956751762044405,
           "rotation": 0,
-          "target": "2-yard-1"
+          "target": "2--1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "1-cafe-at-the-entrance",
-      "name": "Cafe at the entrance",
+      "id": "1---",
+      "name": "Хүлээлгийн кофе шоп",
       "levels": [
         {
           "tileSize": 256,
@@ -79,14 +79,14 @@ var APP_DATA = {
           "yaw": -2.889247820401817,
           "pitch": 0.07423072244583828,
           "rotation": 0,
-          "target": "2-yard-1"
+          "target": "2--1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "2-yard-1",
-      "name": "yard (1)",
+      "id": "2--1",
+      "name": "Талбай (1)",
       "levels": [
         {
           "tileSize": 256,
@@ -121,32 +121,32 @@ var APP_DATA = {
           "yaw": -0.4922393712049242,
           "pitch": 0.07017686879052398,
           "rotation": 0,
-          "target": "0-gate-entrance"
+          "target": "0--"
         },
         {
-          "yaw": 2.7938494750555325,
-          "pitch": 0.19238790502165237,
+          "yaw": 2.7938064457557097,
+          "pitch": 0.19440974881498896,
           "rotation": 0,
-          "target": "3-yard-2"
+          "target": "3--2"
         },
         {
           "yaw": 1.5236847915936664,
           "pitch": -0.05682334729542049,
           "rotation": 0,
-          "target": "15-room-six-1"
+          "target": "15---1"
         },
         {
           "yaw": 0.9348274887981365,
           "pitch": -0.0855007140365931,
           "rotation": 0,
-          "target": "1-cafe-at-the-entrance"
+          "target": "1---"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "3-yard-2",
-      "name": "yard (2)",
+      "id": "3--2",
+      "name": "Талбай (2)",
       "levels": [
         {
           "tileSize": 256,
@@ -181,38 +181,38 @@ var APP_DATA = {
           "yaw": -0.06784796333452903,
           "pitch": 0.1757305834253735,
           "rotation": 0,
-          "target": "2-yard-1"
+          "target": "2--1"
         },
         {
           "yaw": -2.866022157111477,
           "pitch": 0.1166145923384665,
           "rotation": 0,
-          "target": "4-yard-3"
+          "target": "4--3"
         },
         {
           "yaw": 2.2552093288083483,
           "pitch": -0.09177399427924726,
           "rotation": 0,
-          "target": "14-room-five"
+          "target": "14--"
         },
         {
           "yaw": -1.525446491280313,
           "pitch": -0.10718577189782685,
           "rotation": 0,
-          "target": "18-entrance-to-the-residential-complex"
+          "target": "18--"
         },
         {
           "yaw": -1.0434468409943722,
           "pitch": -0.3485489438050351,
           "rotation": 0,
-          "target": "26-living-quarters-two-4"
+          "target": "26----2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "4-yard-3",
-      "name": "yard (3)",
+      "id": "4--3",
+      "name": "Талбай (3)",
       "levels": [
         {
           "tileSize": 256,
@@ -247,32 +247,32 @@ var APP_DATA = {
           "yaw": -1.750665523226747,
           "pitch": 0.13329634673778834,
           "rotation": 0,
-          "target": "3-yard-2"
+          "target": "3--2"
         },
         {
           "yaw": 1.5426547670187132,
           "pitch": 0.1083494509427041,
           "rotation": 0,
-          "target": "5-yard-4"
+          "target": "5--4"
         },
         {
           "yaw": 0.04712329868451093,
           "pitch": -0.12422496129920546,
           "rotation": 0,
-          "target": "11-room-four-1"
+          "target": "11---1"
         },
         {
           "yaw": -3.03515715194537,
           "pitch": -0.06662933321926268,
           "rotation": 0,
-          "target": "17-living-quarters-one"
+          "target": "17--"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "5-yard-4",
-      "name": "yard (4)",
+      "id": "5--4",
+      "name": "Талбай (4)",
       "levels": [
         {
           "tileSize": 256,
@@ -307,32 +307,32 @@ var APP_DATA = {
           "yaw": -1.6735732926740994,
           "pitch": 0.12582804155704963,
           "rotation": 0,
-          "target": "4-yard-3"
+          "target": "4--3"
         },
         {
           "yaw": 1.9619291685036266,
           "pitch": 0.055411446277922494,
           "rotation": 0,
-          "target": "6-yard-5"
+          "target": "6--5"
         },
         {
           "yaw": -0.38193659085153975,
           "pitch": -0.2964326692633481,
           "rotation": 0,
-          "target": "9-room-two-1"
+          "target": "9---1"
         },
         {
           "yaw": 0.9405332519966745,
           "pitch": -0.08303988334835033,
           "rotation": 0,
-          "target": "7-room-one-1"
+          "target": "7---1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "6-yard-5",
-      "name": "yard (5)",
+      "id": "6--5",
+      "name": "Талбай (5)",
       "levels": [
         {
           "tileSize": 256,
@@ -367,26 +367,26 @@ var APP_DATA = {
           "yaw": -1.5086424549149324,
           "pitch": 0.08696278128421575,
           "rotation": 0,
-          "target": "5-yard-4"
+          "target": "5--4"
         },
         {
           "yaw": -0.2325800455220488,
           "pitch": -0.06197798920755204,
           "rotation": 0,
-          "target": "7-room-one-1"
+          "target": "7---1"
         },
         {
           "yaw": -1.0427387002810402,
           "pitch": -0.1409660049784165,
           "rotation": 0,
-          "target": "9-room-two-1"
+          "target": "9---1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "7-room-one-1",
-      "name": "room one (1)",
+      "id": "7---1",
+      "name": "Тасалгаа нэг (1)",
       "levels": [
         {
           "tileSize": 256,
@@ -421,20 +421,20 @@ var APP_DATA = {
           "yaw": 0.2985196009717601,
           "pitch": 0.2221243287522121,
           "rotation": 0,
-          "target": "8-room-one-2"
+          "target": "8---2"
         },
         {
           "yaw": 3.037329743771787,
           "pitch": 0.1348681691007627,
           "rotation": 0,
-          "target": "6-yard-5"
+          "target": "6--5"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "8-room-one-2",
-      "name": "room one (2)",
+      "id": "8---2",
+      "name": "Тасалгаа нэг (2)",
       "levels": [
         {
           "tileSize": 256,
@@ -469,20 +469,20 @@ var APP_DATA = {
           "yaw": -2.5430777817264154,
           "pitch": 0.30331076573555293,
           "rotation": 0,
-          "target": "7-room-one-1"
+          "target": "7---1"
         },
         {
           "yaw": -2.6967567997188056,
           "pitch": -0.015432467298179375,
           "rotation": 0,
-          "target": "6-yard-5"
+          "target": "6--5"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "9-room-two-1",
-      "name": "room two (1)",
+      "id": "9---1",
+      "name": "Тасалгаа хоёр (1)",
       "levels": [
         {
           "tileSize": 256,
@@ -517,20 +517,20 @@ var APP_DATA = {
           "yaw": 0.9154878092479581,
           "pitch": 0.21831604447814712,
           "rotation": 0,
-          "target": "10-room-two-2"
+          "target": "10---2"
         },
         {
           "yaw": -1.7973334110662993,
           "pitch": -0.08695081406136218,
           "rotation": 0,
-          "target": "5-yard-4"
+          "target": "5--4"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "10-room-two-2",
-      "name": "room two (2)",
+      "id": "10---2",
+      "name": "Тасалгаа хоёр (2)",
       "levels": [
         {
           "tileSize": 256,
@@ -565,14 +565,14 @@ var APP_DATA = {
           "yaw": 2.6273093730767316,
           "pitch": 0.19196265232262633,
           "rotation": 0,
-          "target": "9-room-two-1"
+          "target": "9---1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "11-room-four-1",
-      "name": "room four (1)",
+      "id": "11---1",
+      "name": "Тасалгаа гурав (1)",
       "levels": [
         {
           "tileSize": 256,
@@ -607,20 +607,20 @@ var APP_DATA = {
           "yaw": 2.4770436805467266,
           "pitch": 0.24851381829644836,
           "rotation": 0,
-          "target": "12-room-four-2"
+          "target": "12---2"
         },
         {
           "yaw": -0.09418469899168613,
           "pitch": -0.04716267768119309,
           "rotation": 0,
-          "target": "4-yard-3"
+          "target": "4--3"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "12-room-four-2",
-      "name": "room four (2)",
+      "id": "12---2",
+      "name": "Тасалгаа гурав (2)",
       "levels": [
         {
           "tileSize": 256,
@@ -655,26 +655,26 @@ var APP_DATA = {
           "yaw": 1.7766788496124537,
           "pitch": 0.019408518101300842,
           "rotation": 0,
-          "target": "13-room-four--utility-room"
+          "target": "13----"
         },
         {
           "yaw": -0.01988643601013429,
           "pitch": 0.1913908306421046,
           "rotation": 0,
-          "target": "11-room-four-1"
+          "target": "11---1"
         },
         {
           "yaw": 0.20100042702225807,
           "pitch": -0.085160694286877,
           "rotation": 0,
-          "target": "4-yard-3"
+          "target": "4--3"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "13-room-four--utility-room",
-      "name": "room four , utility room",
+      "id": "13----",
+      "name": "Тасалгаа гурав, техникийн өрөө",
       "levels": [
         {
           "tileSize": 256,
@@ -709,14 +709,14 @@ var APP_DATA = {
           "yaw": 2.3501574238445526,
           "pitch": 0.08422237541797628,
           "rotation": 0,
-          "target": "12-room-four-2"
+          "target": "12---2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "14-room-five",
-      "name": "room five",
+      "id": "14--",
+      "name": "Тасалгаа дөрөв",
       "levels": [
         {
           "tileSize": 256,
@@ -751,14 +751,14 @@ var APP_DATA = {
           "yaw": 3.128149942838151,
           "pitch": 0.013962682454643272,
           "rotation": 0,
-          "target": "3-yard-2"
+          "target": "3--2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "15-room-six-1",
-      "name": "room six (1)",
+      "id": "15---1",
+      "name": "Тасалгаа тав (1)",
       "levels": [
         {
           "tileSize": 256,
@@ -793,20 +793,20 @@ var APP_DATA = {
           "yaw": -1.3327954092691119,
           "pitch": 0.18555200587265297,
           "rotation": 0,
-          "target": "16-room-six-2"
+          "target": "16---2"
         },
         {
           "yaw": 1.737409137575332,
           "pitch": -0.06589954517792229,
           "rotation": 0,
-          "target": "2-yard-1"
+          "target": "2--1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "16-room-six-2",
-      "name": "room six (2)",
+      "id": "16---2",
+      "name": "Тасалгаа тав (2)",
       "levels": [
         {
           "tileSize": 256,
@@ -841,20 +841,20 @@ var APP_DATA = {
           "yaw": 1.993407338758706,
           "pitch": 0.22716843662025354,
           "rotation": 0,
-          "target": "15-room-six-1"
+          "target": "15---1"
         },
         {
           "yaw": 2.0544727013482067,
           "pitch": -0.051265266410217336,
           "rotation": 0,
-          "target": "2-yard-1"
+          "target": "2--1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "17-living-quarters-one",
-      "name": "living quarters one",
+      "id": "17--",
+      "name": "Ажилчдын өрөө",
       "levels": [
         {
           "tileSize": 256,
@@ -889,14 +889,14 @@ var APP_DATA = {
           "yaw": 2.993868772270856,
           "pitch": 0.055656817034375194,
           "rotation": 0,
-          "target": "4-yard-3"
+          "target": "4--3"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "18-entrance-to-the-residential-complex",
-      "name": "Entrance to the residential complex",
+      "id": "18--",
+      "name": "Үүдний хэсэг",
       "levels": [
         {
           "tileSize": 256,
@@ -931,32 +931,32 @@ var APP_DATA = {
           "yaw": -1.5749365535377837,
           "pitch": 0.1845378046410513,
           "rotation": 0,
-          "target": "21-staircase-to-the-second-floor"
+          "target": "21--"
         },
         {
           "yaw": 1.3601828933438398,
           "pitch": 0.14843563885090205,
           "rotation": 0,
-          "target": "19-dining-room"
+          "target": "19---"
         },
         {
           "yaw": 0.2969511668952478,
           "pitch": 0.07752797217014695,
           "rotation": 0,
-          "target": "20-kitchen"
+          "target": "20--"
         },
         {
           "yaw": 3.1307430455585017,
           "pitch": 0.2009716911275703,
           "rotation": 0,
-          "target": "3-yard-2"
+          "target": "3--2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "19-dining-room",
-      "name": "Dining room",
+      "id": "19---",
+      "name": "Ажилчдын цайны газар",
       "levels": [
         {
           "tileSize": 256,
@@ -991,14 +991,14 @@ var APP_DATA = {
           "yaw": 1.9437358415438464,
           "pitch": 0.09568724155811026,
           "rotation": 0,
-          "target": "18-entrance-to-the-residential-complex"
+          "target": "18--"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "20-kitchen",
-      "name": "Kitchen",
+      "id": "20--",
+      "name": "Гал тогоо",
       "levels": [
         {
           "tileSize": 256,
@@ -1033,14 +1033,14 @@ var APP_DATA = {
           "yaw": 1.4706901819281022,
           "pitch": 0.08467514704163115,
           "rotation": 0,
-          "target": "18-entrance-to-the-residential-complex"
+          "target": "18--"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "21-staircase-to-the-second-floor",
-      "name": "Staircase to the second floor",
+      "id": "21--",
+      "name": "Дээшээ шат",
       "levels": [
         {
           "tileSize": 256,
@@ -1075,26 +1075,26 @@ var APP_DATA = {
           "yaw": -1.3637855309443019,
           "pitch": 0.5880003586768829,
           "rotation": 0,
-          "target": "18-entrance-to-the-residential-complex"
+          "target": "18--"
         },
         {
           "yaw": -1.515346584288352,
           "pitch": 0.19416080843433114,
           "rotation": 0,
-          "target": "19-dining-room"
+          "target": "19---"
         },
         {
           "yaw": -3.064751023691217,
           "pitch": -0.27406036837301073,
           "rotation": 0,
-          "target": "22-living-quarters-two-1"
+          "target": "22-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "22-living-quarters-two-1",
-      "name": "living quarters two (1)",
+      "id": "22-",
+      "name": "Коридор",
       "levels": [
         {
           "tileSize": 256,
@@ -1129,32 +1129,32 @@ var APP_DATA = {
           "yaw": -0.29903825810463047,
           "pitch": 0.7709065108278761,
           "rotation": 0,
-          "target": "21-staircase-to-the-second-floor"
+          "target": "21--"
         },
         {
           "yaw": -1.4130361438840637,
           "pitch": 0.3864703664236693,
           "rotation": 0,
-          "target": "23-bathroom"
+          "target": "23---"
         },
         {
           "yaw": -2.2715411006896673,
           "pitch": 0.26792602813619304,
           "rotation": 0,
-          "target": "24-living-quarters-two-2"
+          "target": "24--"
         },
         {
           "yaw": -2.9141499462299087,
           "pitch": 0.38868645001653235,
           "rotation": 0,
-          "target": "25-living-quarters-two-3"
+          "target": "25----1"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "23-bathroom",
-      "name": "bathroom",
+      "id": "23---",
+      "name": "Ариун цэврийн өрөө",
       "levels": [
         {
           "tileSize": 256,
@@ -1189,14 +1189,14 @@ var APP_DATA = {
           "yaw": 2.250213222206078,
           "pitch": 0.4530845829361301,
           "rotation": 0,
-          "target": "22-living-quarters-two-1"
+          "target": "22-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "24-living-quarters-two-2",
-      "name": "living quarters two (2)",
+      "id": "24--",
+      "name": "Унтлагын өрөө",
       "levels": [
         {
           "tileSize": 256,
@@ -1231,14 +1231,14 @@ var APP_DATA = {
           "yaw": 1.863634732124189,
           "pitch": 0.5284394571454278,
           "rotation": 0,
-          "target": "22-living-quarters-two-1"
+          "target": "22-"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "25-living-quarters-two-3",
-      "name": "living quarters two (3)",
+      "id": "25----1",
+      "name": "Оффис, ажилчдын өрөө (1)",
       "levels": [
         {
           "tileSize": 256,
@@ -1273,20 +1273,20 @@ var APP_DATA = {
           "yaw": 1.3602247317090175,
           "pitch": 0.3858740630299806,
           "rotation": 0,
-          "target": "22-living-quarters-two-1"
+          "target": "22-"
         },
         {
           "yaw": -1.159861517283856,
           "pitch": 0.42062114981810517,
           "rotation": 0,
-          "target": "26-living-quarters-two-4"
+          "target": "26----2"
         }
       ],
       "infoHotspots": []
     },
     {
-      "id": "26-living-quarters-two-4",
-      "name": "living quarters two (4)",
+      "id": "26----2",
+      "name": "Оффис, ажилчдын өрөө (2)",
       "levels": [
         {
           "tileSize": 256,
@@ -1321,13 +1321,13 @@ var APP_DATA = {
           "yaw": 1.8931351567011738,
           "pitch": 0.39429541127277545,
           "rotation": 0,
-          "target": "25-living-quarters-two-3"
+          "target": "25----1"
         },
         {
           "yaw": -0.589284557816935,
           "pitch": 0.41553996971242846,
           "rotation": 0,
-          "target": "3-yard-2"
+          "target": "3--2"
         }
       ],
       "infoHotspots": []
